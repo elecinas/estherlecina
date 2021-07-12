e35c606044beb66f345e5ce1f0edcb328c8a274c
Esther Lecina <estherlecina@gmail.com>
2021-07-12 16:33:40 +0200

    solved imgShow() problem

--- js/js.js
+++ js/js.js
@@ -1,16 +1,8 @@
-function showImg(img) {
-  var image = new Image();
-  image.src = img;/* asignamos la dirección recibida al objeto imagen */
-  var image_width = image.width;/* obtenemos el ancho de la imagen */
-  /*var pos_x = window.scrollX;*/
-  /*var pos_y = window.scrollY;*/
+function showImg() {
   var x = document.getElementById("modal1");
-  var firstIMG = document.getElementById('modal1').getElementsByTagName('img')[0];
+  //var firstIMG = document.getElementById('modal1').getElementsByTagName('img')[0];
   if (x.style.display === "none") {
     x.style.display = "block";
-    /*window.scroll(pos_x, 100);*/
-    /*firstIMG.style.top = "20px";*/
-    /*firstIMG.style.left = 'calc(50% - ' + image_width + 'px/2)';*/
     $("body").addClass("no_scroll");
   } else {
     x.style.display = "none";
