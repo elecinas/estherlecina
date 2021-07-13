54028f368b5b8a541106bbc95b65fc0f72938ae2
Esther Lecina <estherlecina@gmail.com>
2021-07-13 15:56:08 +0200

    solved the problem of scroll on showImg()

--- js/js.js
+++ js/js.js
@@ -1,11 +1,15 @@
 function showImg(img) {
-  var x = document.getElementById("xfrd_Luna_07_08");
-  //var firstIMG = document.getElementById('xfrd_Luna_07_08').getElementsByTagName('img')[0];
+  var scroll_position = window.scrollY;//guardem la posició actual de l'scroll
+  var x = document.getElementById(img);//obtenim l'element enviat
+  var clase = document.querySelectorAll('.fondo_modal');//seleccionem la classe
+  clase.forEach(element => {
+    element.style.top = Math.round(scroll_position) + "px";//possició de l'scroll := posició de la imatge
+  });
   if (x.style.display === "none") {
-    x.style.display = "block";
-    $("body").addClass("no_scroll");
+    x.style.display = "block";//ensenyem l'imatge
+    $("body").addClass("no_scroll");//bloquegem l'scroll
   } else {
-    x.style.display = "none";
-    $("body").removeClass("no_scroll");
+    x.style.display = "none";//amaguem l'imatge
+    $("body").removeClass("no_scroll");//desbloquegem l'scroll
   }
 }
