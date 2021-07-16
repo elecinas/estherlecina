c1d5554b87c538df23caef18cc24bf5df056e996
Esther Lecina <estherlecina@gmail.com>
2021-07-16 16:34:02 +0200

    creado acceso a json, pero con problema con CORS

--- js/js.js
+++ js/js.js
@@ -13,3 +13,22 @@ function showImg(img) {
     $("body").removeClass("no_scroll");//desbloquegem l'scroll
   }
 }
+
+/*cargando data.json */
+
+function readTextFile(file, callback) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.overrideMimeType("application/json");
+  rawFile.open("GET", file, true);
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(rawFile.responseText);
+      }
+  }
+  rawFile.send(null);
+}
+
+readTextFile("data.json", function(text){
+  var data = JSON.parse(text);
+  console.log(data);
+});
